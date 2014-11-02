8fbfd89b23b3ce477f64fd0c5f3dd7b9274cb451
yourgamesbeover <yourgamesbeover@gmail.com>
2014-11-01 21:41:40 -0400

    movement controls test 1

---
+++ Assets/Scripts/RotationManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationManager : MonoBehaviour {
+	public Rigidbody rgb;
+	
+	public Vector3 force;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float inputX = Input.GetAxis("LSx") * force.x;
+		float inputY = Input.GetAxis("LSy") * force.y;
+		float inputZ = Input.GetAxis("Bumpers") * force.z;
+
+		Vector3 forceVector = new Vector3(inputY,inputX,inputZ);
+		
+		rgb.AddRelativeTorque(forceVector);
+	}
+}
---
+++ Assets/Scripts/StrafeManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrafeManager : MonoBehaviour {
+	public Rigidbody rgb;
+
+	public float force;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float inputX = -Input.GetAxis("RSx");
+		float inputY = Input.GetAxis("RSy");
+
+		Vector3 forceVector = new Vector3(inputX,inputY,0)*force;
+
+		rgb.AddRelativeForce(forceVector);
+	}
+}
