98055fdbb37bb98490952f008e567baa11903df4
yourgamesbeover <yourgamesbeover@gmail.com>
2014-11-04 19:33:59 -0500

    re-added dynamic radar

--- Assets/Scripts/RadarMount.cs
+++ Assets/Scripts/RadarMount.cs
@@ -11,21 +11,6 @@ public class RadarMount : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(!cameraToPosition.enabled){//super hack time!
-			Debug.LogWarning("Radar camera is disabled! searching for child...");
-			Camera newCam = null;
-			foreach(Transform child in cameraToPosition.transform){//this code
-				newCam = cameraToPosition.transform.GetChild(0).GetComponent<Camera>();
-				if(newCam)break;
-			}
-			if(newCam && newCam.enabled){
-				Debug.LogWarning("Radar camera changed from "+cameraToPosition.name+" to its child, "+newCam.name+".");
-				cameraToPosition = newCam;
-			}else{
-				Debug.LogWarning("No valid child camera found!");
-			}
-		}
-
 		Vector3 topRightScreenPoint = mainCamera.WorldToViewportPoint(topRight.position);
 		Vector3 bottomLeftScreenPoint = mainCamera.WorldToViewportPoint(bottomLeft.position);
 		float x = bottomLeftScreenPoint.x;
@@ -37,6 +22,7 @@ public class RadarMount : MonoBehaviour {
 
 		DebugHUD.setValue("Radar Rect",newView);
 
-		cameraToPosition.rect = newView;
+		foreach(Camera cam in cameraToPosition.GetComponentsInChildren<Camera>())
+			cam.rect = newView;
 	}
 }
