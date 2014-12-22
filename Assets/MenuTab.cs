71c89693031ff823de55967a3694e7c450ab9a4c
freshvictory <jsren1@verizon.net>
2014-12-22 15:17:23 -0500

    menu stuff

---
+++ Assets/MenuTab.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuTab {
+
+//	[Header("Title:")]
+//	public string[] title;
+//	public Font titleFont;
+//	public int titleFontSize;
+//	public Color titleTextColor;
+//	public Vector2 titleAnchorMin;
+//	public Vector2 titleAnchorMax;
+//	[Space(10)]
+//	[Header("Options:")]
+//	public string[] options;
+//	public Font optionFont;
+//	public int optionFontSize;
+//	public Color optionTextColor;
+//	public Vector2 optionAnchorMin;
+//	public Vector2 optionAnchorMax;
+//	public Vector2 optionOffset;
+//	public Sprite optionSprite;
+//	public Sprite optionSpriteTransparent;
+//	[Space(10)]
+//	[Header("Tabs:")]
+	public string[] text;
+	public Font tabFont;
+	public int tabFontSize;
+	public Color tabTextColor;
+	public Vector2 tabAnchorMin;
+	public Vector2 tabAnchorMax;
+	public Vector2 tabOffset;
+	public GameObject tab;
+	public float tabObjectOffset;
+}
