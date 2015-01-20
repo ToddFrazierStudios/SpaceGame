9b9069a31b70a781c8578b80d6fed7e0f53aafeb
yourgamesbeover <yourgamesbeover@gmail.com>
2015-01-19 19:19:12 -0500

    Controller.cs

---
+++ Assets/Scripts/Input/Controller.cs
@@ -0,0 +1,107 @@
+﻿public class Controller {
+	public float LookX{
+		get{
+			return 0.0f;
+		}
+	}
+	public float LookY{
+		get{
+			return 0.0f;
+		}
+	}
+	public float StrafeX{
+		get{
+			return 0.0f;
+		}
+	}
+	public float StrafeY{
+		get{
+			return 0.0f;
+		}
+	}
+	public bool Fire{
+		get{
+			return false;
+		}
+	}
+	public bool AltFire{
+		get{
+			return false;
+		}
+	}
+
+	public float Roll{
+		get{
+			return 0.0f;
+		}
+	}
+	public float Throttle{
+		get{
+			return 0.0f;
+		}
+	}
+
+	public bool Dampeners{
+		get{
+			return false;
+		}
+	}
+	public bool Boost{
+		get{
+			return false;
+		}
+	}
+	public bool RadarButton{
+		get{
+			return false;
+		}
+	}
+	public bool CameraButton{
+		get{
+			return false;
+		}
+	}
+	public bool PauseButton{
+		get{
+			return false;
+		}
+	}
+	public bool TargetButton{
+		get{
+			return false;
+		}
+	}
+	public bool NextWeapon{
+		get{
+			return false;
+		}
+	}
+	public bool PreviousWeapon{
+		get{
+			return false;
+		}
+	}
+	public bool SelectWeapon1{
+		get{
+			return false;
+		}
+	}
+	public bool SelectWeapon2{
+		get{
+			return false;
+		}
+	}
+	public bool SelectWeapon3{
+		get{
+			return false;
+		}
+	}
+	public bool SelectWeapon4{
+		get{
+			return false;
+		}
+	}
+	public void SetVibration(float left, float right){
+
+	}
+}
