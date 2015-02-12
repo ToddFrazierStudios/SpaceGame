5997d929b3c0f9156ddf68abf0d287cc4e19ade5
yourgamesbeover <yourgamesbeover@gmail.com>
2015-02-12 01:32:57 -0500

    The start of controller profiles

--- Assets/Scripts/Input/ControllerProfile.cs
+++ Assets/Scripts/Input/ControllerProfile.cs
@@ -1,20 +1,56 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using SimpleJSON;
+using System.IO;
+using System;
 
 public class ControllerProfile {
-    public Dictionary<string, string> table;
-    public string[] names;
-    public bool osxTriggerCompNeeded;
-    public bool analogDPad;
-    public bool invertedRightStick;
+    private Dictionary<string, string> table;
+    private string[] names;
+    private bool osxTriggerCompNeeded;
+    private bool analogDPad;
+    private bool invertedRightStick;
+    private Platform platform;
 
-    private ControllerProfile() {
-        table = new Dictionary<string, string>();
+    private ControllerProfile(Dictionary<string, string> tab, string[] nams, bool osx, bool dpad, bool invRS, Platform plat) {
+        table = tab;
+        names = nams;
+        osxTriggerCompNeeded = osx;
+        analogDPad = dpad;
+        invertedRightStick = invRS;
+        platform = plat;
     }
 
+    public string this[string s] {
+        get {
+            return table[s];
+        }
+    }
 
+    public bool hasName(string other) {
+        foreach (string name in names) {
+            if (name == other) return true;
+        }
+        return false;
+    }
 
+    public bool OSXTriggerCompNeeded {
+        get { return osxTriggerCompNeeded; }
+    }
+    public bool AnalogDPad {
+        get { return analogDPad; }
+    }
+    public bool InvertedRightStick {
+        get { return invertedRightStick; }
+    }
+
+    public Platform PlatformRequired {
+        get { return platform; }
+    }
+
+    public enum Platform {
+        ANY,WINDOWS,OSX,OTHER
+    }
 
     ////STATIC////
     private static List<ControllerProfile> profiles;
@@ -22,27 +58,71 @@ public class ControllerProfile {
         TextAsset[] textAssets = Resources.LoadAll<TextAsset>("ControllerProfiles");
         Debug.Log(textAssets.Length + " TextAsset(s) found!");
         foreach (TextAsset ta in textAssets) {
+            Debug.Log("Parsing TextAsset " + ta.name);
             parseJSON(ta.text);
         }
     }
+
+    private static void loadNonBundledProfiles() {
+        string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(),"*.json");
+        Debug.Log(files.Length + " external json file(s) found: " + string.Join(",", files));
+        foreach (string file in files) {
+            parseJSON(File.ReadAllText(file));
+        }
+    }
+
+    public static void ReloadProfiles() {
+        if (profiles == null) 
+            profiles = new List<ControllerProfile>();
+        else
+          profiles.Clear();
+
+        loadBundledProfiles();
+        loadNonBundledProfiles();
+    }
+
     private static void parseJSON(string json) {
         JSONNode root = JSON.Parse(json);
-        ControllerProfile profile = new ControllerProfile();
         int nameCount = root["names"].AsArray.Count;
-        profile.names = new string[nameCount];
+        string[] names = new string[nameCount];
         for (int i = 0; i<nameCount; i++) {
-            profile.names[i] = root["names"].AsArray[i];
+            names[i] = root["names"].AsArray[i];
+            Debug.Log("Name found: " + names[i]);
         }
-        profile.osxTriggerCompNeeded = root["OSXTriggerCompensationRequired"].AsBool;
-        profile.analogDPad = root["analogDpad"].AsBool;
-        profile.invertedRightStick = root["invertedRightStick"].AsBool;
+        bool osxTriggerCompNeeded = root["OSXTriggerCompensationRequired"].AsBool;
+        Debug.Log("OSX trigger comp is " + (osxTriggerCompNeeded ? "" : "not") + " needed");
+
+        bool analogDPad = root["analogDpad"].AsBool;
+        Debug.Log("DPad is " + (analogDPad ? "" : "not") + " analog");
+
+        bool invertedRightStick = root["invertedRightStick"].AsBool;
+        Debug.Log("Right stick is " + (invertedRightStick ? "" : "not") + " inverted");
+
+        Platform platform;
+        switch (root["platform"]) {
+            case "WIN":
+                platform = Platform.WINDOWS;
+                break;
+            case "OSX":
+                platform = Platform.OSX;
+                break;
+            case "OTHER":
+                platform = Platform.OTHER;
+                break;
+            default:
+                platform = Platform.ANY;
+                break;
+        }
+        Debug.Log("Set Platform to " + Enum.GetName(typeof(Platform), platform));
+
         //time to fill out the table...
-        
-        foreach (JSONClass node in root["table"].AsObject) {
-            
+        Dictionary<string, string> table = new Dictionary<string, string>();
+        Debug.Log(root["table"].GetType().Name);
+        foreach (KeyValuePair<string,JSONNode> pair in root["table"] as JSONClass) {
+            Debug.Log(string.Format("Adding pair ({0},{1}) to table", pair.Key, pair.Value));
+            table.Add(pair.Key, pair.Value.Value);
         }
 
-        profiles.Remove(profile);
-        profiles.Add(profile);
+        profiles.Add(new ControllerProfile(table, names, osxTriggerCompNeeded, analogDPad, invertedRightStick, platform));
     }
 }
